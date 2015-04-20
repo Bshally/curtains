62483dcb6ab6075eacd030d9ef071ec822e63828
Bshally <brandon>
2015-04-20 16:49:46 -0400

    got into positions,
    setting lables
    NOTE: use getElementById() not $(“#Id”)

--- js/main.js
+++ js/main.js
@@ -1,19 +1,34 @@
 $(document).ready(function(){
-  var height = document.getElementById("height");
-  var width = document.getElementById("width");
-	var slider = document.getElementById("ratio");
+    //Get input elements
+    var height = document.getElementById("height");
+    var width = document.getElementById("width");
+    var slider = document.getElementById("ratio");
+    //Get diagram labels
+    var topLabel = document.getElementsByClassName("toplabel");
+    var bottomLabel = document.getElementsByClassName("bottom-label");
+    var currentLabel = document.getElementsByClassName("current-label");
+    //SVG OBJ's
+    var vinyl = $("#vinyl");
+    //END SVG OBJ's
 	slider.max = 10;
   var sqFt;
         $('form').on('submit', function(event){
           event.preventDefault();
             var newMax = parseInt(height.value);
-  					slider.max = newMax;
+            slider.max = newMax;
           	sqFt = parseInt(height.value ) * parseInt(width.value);
-  					        	
-        });
-  
-    });
 
+        });
+
+
+
+
+    //TweenLite.to(vinyl, 3, {
+    //    //attr:{height:250}
+    //
+    //});
+
+    });
 
 
 
